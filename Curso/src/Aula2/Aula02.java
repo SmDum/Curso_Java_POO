1d7c1786aace642043ec569215001569744b1283
Samuel de Moraes Delgado <samueldemoraesdelgado2005@gmail.com>
2025-08-18 07:37:39 -0300

    Curso POO

--- Curso/src/Aula2/Aula02.java
+++ Curso/src/Aula2/Aula02.java
@@ -3,18 +3,13 @@ package Aula2;
 public class Aula02 {
     public static void main(String[] args){
         Caneta c1 = new Caneta();
+        c1.modelo = "BIC Cristal";
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
-        c1.tampada = false;
+        // c1.ponta = 0.5f; // Atributo privado, não acessível fora da classe
+        c1.carga = 80;
+        //c1.tampada = false;
+
         c1.tampar();
         c1.status();
-        c1.rabiscar();
-
-        Caneta c2 = new Caneta();
-        c2.modelo = "BIC";
-        c2.cor = "Preta";
-        c2.destampar();
-        c2.status();
-        c2.rabiscar();
     }
 }
--- Curso/src/Aula2/Caneta.java
+++ Curso/src/Aula2/Caneta.java
@@ -1,21 +1,21 @@
 package Aula2;
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
 
-    void status(){
+    public void status(){
         System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga);
-        System.out.println("Uma caneta " + this.cor);
         System.out.println("Está tampada? " + this.tampada);
 
     }
 
-    void rabiscar(){
+    public void rabiscar(){
         if(this.tampada){
             System.out.println("Erro! Não posso rabiscar");
         } else {
@@ -23,11 +23,11 @@ public class Caneta {
         }
     }
 
-    void tampar(){
+    public void tampar(){
         this.tampada = true;
     }
 
-    void destampar(){
+    public void destampar(){
         this.tampada = false;
     }
 }
